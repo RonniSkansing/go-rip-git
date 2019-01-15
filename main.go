041028ceab06a59affa0668e4d97c5251bce7d78
Ronni Skansing <rskansing@gmail.com>
2019-01-15 22:17:31 +0100

    correct import path for lib

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ import (
 	"flag"
 	"net/url"
 
-	"github.com/RonnieSkansing/go-rip-git/client"
-	"github.com/RonnieSkansing/go-rip-git/logger"
-	"github.com/RonnieSkansing/go-rip-git/scraper"
+	"github.com/RonniSkansing/go-rip-git/client"
+	"github.com/RonniSkansing/go-rip-git/logger"
+	"github.com/RonniSkansing/go-rip-git/scraper"
 )
 
 func main() {
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -14,7 +14,7 @@ import (
 	"strconv"
 	"sync"
 
-	"github.com/RonnieSkansing/go-rip-git/logger"
+	"github.com/RonniSkansing/go-rip-git/logger"
 	"path/filepath"
 )
 
